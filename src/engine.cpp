19cc652826485551a6dafe39ae57e5740b194662
cyrus <cyrus.behr@gmail.com>
2023-05-05 17:08:47 -0700

    Fixed error message

--- src/engine.cpp
+++ src/engine.cpp
@@ -284,8 +284,8 @@ bool Engine::runInference(const std::vector<std::vector<cv::cuda::GpuMat>> &inpu
             input.cols != dims.d[2]) {
             std::cout << "===== Error =====" << std::endl;
             std::cout << "Input does not have correct size!" << std::endl;
-            std::cout << "Expected: (" << dims.d[1] << ", " << dims.d[2] << ", "
-                      << dims.d[3] << ")" << std::endl;
+            std::cout << "Expected: (" << dims.d[0] << ", " << dims.d[1] << ", "
+                      << dims.d[2] << ")" << std::endl;
             std::cout << "Got: (" << input.channels() << ", " << input.rows << ", " << input.cols << ")" << std::endl;
             std::cout << "Ensure you resize your input image to the correct size" << std::endl;
             return false;
