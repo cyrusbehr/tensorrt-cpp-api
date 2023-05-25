2718693ec8e80c9b94d3f20e122a18f35981d945
cyrus <cyrus.behr@gmail.com>
2023-05-24 21:28:10 -0700

    Added ccache to speed rebuilds

---
+++ cmake/ccache.cmake
@@ -0,0 +1,7 @@
+find_program(CCACHE_PROGRAM ccache)
+if(CCACHE_PROGRAM)
+    set_property(GLOBAL PROPERTY RULE_LAUNCH_COMPILE "${CCACHE_PROGRAM}")
+    message(STATUS "ccache: found")
+else()
+    message(STATUS "ccache: not found")
+endif()
